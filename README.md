671fceb84c773f803f586b21210fb412a76c89e6
José Pedro Dias <jose.pedro.dias@gmail.com>
2016-10-23 12:53:06 +0100

    v1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 based on: <https://dreamdom.github.io/speechrec.html>
+
+
+
+TODO:
+
+check rate? 16000?
+check comparison alg
+serialize/deserialize float arrays
--- fetch.sh
+++ fetch.sh
@@ -3,3 +3,6 @@
 wget https://dreamdom.github.io/adapter.js
 wget https://dreamdom.github.io/JsSpeechRecognizer.js
 wget https://dreamdom.github.io/speechrec.html
+
+wget https://raw.githubusercontent.com/spencertipping/flotsam/master/js/flotsam32.js
+wget https://raw.githubusercontent.com/spencertipping/flotsam/master/js/flotsam.js
